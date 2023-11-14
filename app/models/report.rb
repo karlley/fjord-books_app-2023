cac3a4b7979ff62ca3fbdb7932c32bf667ce4433
karlley <karlley.jp@gmail.com>
2023-11-15 08:30:58 +0900

    日報更新時にメンションのレコードを更新

--- app/models/report.rb
+++ app/models/report.rb
@@ -20,15 +20,35 @@ class Report < ApplicationRecord
     created_at.to_date
   end
 
-  def create_mentions
+  def extract_mention_target_ids
     pattern = %r{http://localhost:3000/reports/([0-9０-９]+)}
-    mention_target_ids = content.scan(pattern).map do |match|
+    content.scan(pattern).map do |match|
       match_value = match.first
       converted_value = match_value&.tr('０-９', '0-9')&.to_i
       converted_value || match_value
     end.uniq
-    mention_target_ids.each do |target_id|
+  end
+
+  def create_mentions
+    create_mention_target_ids = extract_mention_target_ids
+    create_mention_target_ids.each do |target_id|
       active_mentions.create(mention_target_id: target_id)
+    end
+  end
+
+  def update_mentions
+    mention_target_ids = extract_mention_target_ids
+    create_mention_target_ids = mention_target_ids - mentioning_report_ids
+    delete_mention_target_ids = mentioning_report_ids - mention_target_ids
+
+    transaction do
+      create_mention_target_ids.each do |target_id|
+        active_mentions.create!(mention_target_id: target_id)
+      end
+      delete_mention_target_ids.each do |target_id|
+        target_mention = active_mentions.find_by!(mention_target_id: target_id)
+        target_mention.destroy!
+      end
     end
   end
 end
