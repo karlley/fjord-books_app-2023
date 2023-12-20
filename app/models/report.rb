67014352d06d697321b6faf57b3a8b75fe04a9b7
karlley <karlley.jp@gmail.com>
2023-12-20 13:12:34 +0900

    変数名の修正、日報の取得を一括取得に変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,15 +4,17 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :active_mentions, class_name: 'Mention', foreign_key: 'mention_source_id', dependent: :destroy, inverse_of: :mention_source
-  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'mention_target_id', dependent: :destroy, inverse_of: :mention_target
-  has_many :mentioning_reports, through: :active_mentions, source: :mention_target
-  has_many :mentioned_reports, through: :passive_mentions, source: :mention_source
+  has_many :mentioners, class_name: 'Mention', foreign_key: 'mention_source_id', dependent: :destroy, inverse_of:
+    :mention_source
+  has_many :mentionees, class_name: 'Mention', foreign_key: 'mention_target_id', dependent: :destroy, inverse_of:
+    :mention_target
+  has_many :mentioning_reports, through: :mentioners, source: :mention_target
+  has_many :mentioned_reports, through: :mentionees, source: :mention_source
 
   validates :title, presence: true
   validates :content, presence: true
 
-  after_create :create_mentions!
+  after_create :update_mentions!
   after_update :update_mentions!
 
   def editable?(target_user)
@@ -28,32 +30,24 @@ class Report < ApplicationRecord
   def extract_mention_target_ids
     url_regexp = %r{http://localhost:3000/reports/([0-9０-９]+)(?!.*?/edit)}
     content.scan(url_regexp).map do |match|
-      report_id = match.first
-      numeric_report_id = report_id&.tr('０-９', '0-9')&.to_i
-      numeric_report_id || report_id
+      raw_report_id = match.first
+      numeric_report_id = raw_report_id&.tr('０-９', '0-9')&.to_i
+      numeric_report_id || raw_report_id
     end.uniq
-  end
-
-  def create_mentions!
-    create_mention_target_ids = extract_mention_target_ids
-    create_mention_target_ids.each do |target_id|
-      active_mentions.create!(mention_target_id: target_id)
-    end
   end
 
   def update_mentions!
     mention_target_ids = extract_mention_target_ids
-    create_mention_target_ids = mention_target_ids - mentioning_report_ids
-    delete_mention_target_ids = mentioning_report_ids - mention_target_ids
+    new_mention_ids = mention_target_ids - mentioning_report_ids
+    obsolete_mention_ids = mentioning_report_ids - mention_target_ids
+    obsolete_mentions = mentioners.where(mention_target_id: obsolete_mention_ids)
 
     transaction do
-      create_mention_target_ids.each do |target_id|
-        active_mentions.create!(mention_target_id: target_id)
+      new_mention_ids.each do |new_mention_id|
+        mentioners.create!(mention_target_id: new_mention_id)
       end
-      delete_mention_target_ids.each do |target_id|
-        target_mention = active_mentions.find_by!(mention_target_id: target_id)
-        target_mention.destroy!
-      end
+
+      obsolete_mentions.each(&:destroy!)
     end
   end
 end
