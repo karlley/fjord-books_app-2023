3b521c8e414c081324cd7d6f813aa472d4eee15c
karlley <karlley.jp@gmail.com>
2024-05-20 08:50:57 +0900

    テスト時の値をイメージし易い値に変更 メソッドの返り値を直接値を指定して検証するように修正 日報の更新、削除時のテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,32 +4,78 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   def setup
-    @report_author = User.create!(email: 'foo@example.com', name: 'Foo', password: 'passoword')
+    @report_author = User.create!(email: 'taro@example.com', name: '太郎', password: 'passoword')
     @report = Report.create!(user_id: @report_author.id, title: 'report', content: 'text')
   end
 
   # editable?
   test '日報の投稿者だった場合はtrue、日報の投稿者で無い場合はfalseを返す' do
-    not_report_author = User.create!(email: 'bar@example.com', name: 'Bar', password: 'password')
+    not_report_author = User.create!(email: 'jiro@example.com', name: '次郎', password: 'password')
 
     assert @report.editable?(@report_author)
     assert_not @report.editable?(not_report_author)
   end
 
   # created_on
-  test '日報の作成年月日、作成曜日のみを返す' do
-    assert_equal @report.created_at.to_date, @report.created_on
+  test '日付オブジェクトで日報の作成年月日のみを返す' do
+    assert_instance_of Date, @report.created_on
+    created_at = @report.created_at
+    expected_format = %r{\A#{created_at.year}/#{format('%02d', created_at.month)}/#{format('%02d', created_at.day)}\z}
+    formatted_date = I18n.l(@report.created_on)
+    assert_match expected_format, formatted_date
   end
 
-  # save_mentions
-  test '日報本文にメンション対象の日報URLが含まれていたらメンションが作成される' do
-    assert_equal(0, ReportMention.count)
-    assert_nil ReportMention.find_by(mentioned_by_id: @report.id)
+  # save_mentions、日報作成、メンション作成
+  test '本文にメンション対象の日報URLを含む日報を作成するとメンションが作成される' do
+    assert_equal(0, @report.mentioned_reports.count)
 
     mentioning_report = Report.create!(user_id: @report_author.id, title: 'mention to report', content:
       "http://localhost:3000/reports/#{@report.id}")
 
-    assert_equal(1, ReportMention.count)
-    assert_not_nil ReportMention.find_by(mention_to_id: mentioning_report.id, mentioned_by_id: @report.id)
+    assert_equal(1, @report.mentioned_reports.count)
+    assert_equal(1, mentioning_report.mentioning_reports.count)
+  end
+
+  # save_mentions、日報更新、メンション追加
+  test '本文にメンション対象の日報URLを追加後に日報の更新を行うとメンションが作成される' do
+    mentioning_report = Report.create!(user_id: @report_author.id, title: 'report', content: 'メンションは未作成')
+    assert_equal(0, @report.mentioned_reports.count)
+    assert_equal(0, mentioning_report.mentioning_reports.count)
+
+    mentioning_report.update!(content: "http://localhost:3000/reports/#{@report.id}")
+    assert_equal(1, @report.mentioned_reports.count)
+    assert_equal(1, mentioning_report.mentioning_reports.count)
+  end
+
+  # save_mentions、日報更新、メンション削除
+  test '本文のメンション対象の日報URLを削除後に日報の更新を行うとメンションが削除される' do
+    mentioning_report = Report.create!(user_id: @report_author.id, title: 'report', content: "http://localhost:3000/reports/#{@report.id}")
+    assert_equal(1, @report.mentioned_reports.count)
+    assert_equal(1, mentioning_report.mentioning_reports.count)
+
+    mentioning_report.update!(content: 'メンションの削除')
+    assert_equal(0, @report.mentioned_reports.count)
+    assert_equal(0, mentioning_report.mentioning_reports.count)
+  end
+
+  # save_mentions、日報更新、メンション未更新
+  test '本文のメンション対象の日報URLを変更せずに日報更新を行うとメンションは作成されない' do
+    mentioning_report = Report.create!(user_id: @report_author.id, title: 'report', content: "http://localhost:3000/reports/#{@report.id}")
+    assert_equal(1, @report.mentioned_reports.count)
+    assert_equal(1, mentioning_report.mentioning_reports.count)
+
+    mentioning_report.update!(content: "http://localhost:3000/reports/#{@report.id} メンションは未更新")
+    assert_equal(1, @report.mentioned_reports.count)
+    assert_equal(1, mentioning_report.mentioning_reports.count)
+  end
+
+  test '本文にメンション対象の日報URLが含まれた日報を削除するとメンションが削除される' do
+    mentioning_report = Report.create!(user_id: @report_author.id, title: 'report', content: "http://localhost:3000/reports/#{@report.id}")
+    assert_equal(1, @report.mentioned_reports.count)
+    assert_equal(1, mentioning_report.mentioning_reports.count)
+
+    mentioning_report.update!(content: 'メンションを削除')
+    assert_equal(0, @report.mentioned_reports.count)
+    assert_equal(0, mentioning_report.mentioning_reports.count)
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,10 +5,10 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   # name_or_email
   test 'ユーザ名が未入力の場合はメールアドレスがユーザ名になる' do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
+    user = User.new(email: 'taro@example.com', name: '')
+    assert_equal 'taro@example.com', user.name_or_email
 
-    user.name = 'Foo Bar'
-    assert_equal 'Foo Bar', user.name_or_email
+    user.name = '太郎'
+    assert_equal '太郎', user.name_or_email
   end
 end
